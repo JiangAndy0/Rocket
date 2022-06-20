7189ac864d095e41ec7854eb67e34665b975169c
Andy Jiang <andyjiangart@gmail.com>
2022-06-19 19:06:00 -0500

    add thumbnail gallery and event handlers

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
-let chargeButton = document.getElementById('charge-button');
-let resetButton = document.getElementById('reset-button');
-let progressbar = document.getElementById('progress-bar');
-let progressElement = document.getElementById('progress');
-let rocket = document.getElementById('rocket');
-let rocketArea = document.getElementById('rocket-area');
+const chargeButton = document.getElementById('charge-button');
+const resetButton = document.getElementById('reset-button');
+const progressbar = document.getElementById('progress-bar');
+const progressElement = document.getElementById('progress');
+const rocket = document.getElementById('rocket');
+const rocketArea = document.getElementById('rocket-area');
 
+
+//thumbnail elements
+const thumb1 = document.getElementById('thumb-1');
+const thumb2 = document.getElementById('thumb-2');
+const thumb3 = document.getElementById('thumb-3');
+const thumb4 = document.getElementById('thumb-4');
+const thumb5 = document.getElementById('thumb-5');
+
+const thumbs = [thumb1, thumb2, thumb3, thumb4, thumb5];
+
+//image paths
+const img1Path = ""; //no image, just plain background
+const img2Path = "./resources/city-1.jpg";
+const img3Path = "./resources/forest-1.webp";
+const img4Path = "./resources/forest-2.webp";
+const img5Path = "./resources/mars.jpg";
+
+const imgPaths = [img1Path, img2Path, img3Path, img4Path, img5Path];
+
+//current thumbnail image is the first one
+let currentThumb = thumb1;
 
 let progressPercent = 0;
 let evenIteration = true;
-let backgroundImageIndex = 0;
 
 
 //charges rocket by 10% until it gets to 100%, then launches rocket
@@ -37,8 +57,6 @@ function chargeRocket(){
 
 }
 
-//when the charge button is clicked, the rocket charges
-chargeButton.addEventListener('click', chargeRocket);
 
 //resets progress to 0% and flies a new rocket in
 function resetRocket(){
@@ -50,22 +68,47 @@ function resetRocket(){
     chargeButton.style.display = 'block';
 }
 
+//activates on click on thumbnails, changes image displayed behind rocket
+function changeImage( event ){
+    //Take the focus off the current thumbnail
+    currentThumb.style.border = 'none';
+    currentThumb.style.opacity = '50%';
+    currentThumb.addEventListener('mouseout', thumbMouseout); //allows thumbnail to be de-focused on
+
+    //Put the focus on the new image thumbnail
+    event.target.style.border = '3px solid hsl(240, 30%, 30%)';
+    event.target.style.opacity = '100%';
+    event.target.removeEventListener('mouseout', thumbMouseout); //prevents thumbnail from being defocused
+
+    //set new image thumbnail as current image thumbnail
+    currentThumb = event.target;
+}
+
+//activates on mouseover on thumbnails, changes opacity to 100%
+function thumbMouseover( event ){
+    event.target.style.opacity = '100%';
+}
+
+//activates on mouseout on thumbnails, changes opacity to 50%
+function thumbMouseout( event ){
+    event.target.style.opacity = '50%';
+}
+
+
+
+//
+//when the charge button is clicked, the rocket charges
+chargeButton.addEventListener('click', chargeRocket);
 //when the reset button is clicked, a new rocket flies in 
 resetButton.addEventListener('click', resetRocket);
 
-//change the background image of the rocket area
-function changeBackground() {
-    //array containing directories of background images
-    const backgroundImages = [' ', './resources/city-1.jpg', './resources/forest-1.webp','./resources/forest-2.webp', './resources/mars.jpg'];
-    //iterate background image index
-    backgroundImageIndex++;
-    if (backgroundImageIndex > backgroundImages.length - 1){
-        backgroundImageIndex = 0;
-    }
-
-    rocketArea.style.backgroundImage = `url(${backgroundImages[backgroundImageIndex]})`;
-
+//add event listeners to all the thumbnail elements
+for (thumbElement of thumbs){
+    thumbElement.addEventListener('click', changeImage);
+    thumbElement.addEventListener('mouseover', thumbMouseover);
+    thumbElement.addEventListener('mouseout', thumbMouseout);
 }
 
-//when the rocket area is clicked, its background image changes
-rocketArea.addEventListener('click', changeBackground);
+//put focus on first thumbnail
+currentThumb.style.border = '3px solid hsl(240, 30%, 30%)';
+currentThumb.removeEventListener('mouseout', thumbMouseout);
